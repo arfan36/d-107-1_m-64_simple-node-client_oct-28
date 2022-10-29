662e5344dc4816de2a1002168fc19fdd53e6ec0b
arfan36 <arfanur36@gmail.com>
2022-10-29 10:55:16 +0600

    64-2 (Recap) install node-express and use fetch to load data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:5000/users')
+      .then(res => res.json())
+      .then(data => setUsers(data));
+  }, []);
+
+  const handleAddUser = (event) => {
+    event.preventDefault();
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <form onSubmit={handleAddUser}>
+        <input type="text" name='name' /><br />
+        <input type="email" name="email" /><br />
+        <button type='submit'>Add User</button>
+      </form>
+
+      <h2>Users: {users.length}</h2>
+      <div>
+        {
+          users.map(user => <p key={user.id}>{user.name} : {user.email}</p>)
+        }
+      </div>
     </div>
   );
 }
